24efdf2eab7f729f40c4861a83cf7c02e933cd06
quannghiem756 <62365443+quannghiem756@users.noreply.github.com>
2024-09-11 13:17:54 +0700

    Update user schema

--- models/User.js
+++ models/User.js
@@ -1,18 +1,65 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 50
   },
   password: {
-    type: Boolean,
-    default: false
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 128,
+    select: false
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: mongoose.validator.isEmail,
+      message: 'Please fill a valid email address'
+    }
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
   }
 });
 
-module.exports = mongoose.model('Task', UserSchema);
+UserSchema.pre('save', async function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+  }
+
+  next();
+});
+UserSchema.statics.findByCredentials = async (email, password) => {
+  const user = await this.findOne({ email });
+
+  if (!user) {
+      throw new Error('Unable to login!');
+  }
+
+  const isCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isCorrect) {
+      throw new Error('Unable to login!');
+  }
+
+  return user;
+}
+module.exports = mongoose.model('User', UserSchema);
+
