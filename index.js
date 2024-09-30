ed99a854d8825d4414a7d223730a1ef05596ab7a
CaptainRinoXD <dinhdainhan1@gmail.com>
2024-09-30 23:36:23 +0700

    unkonw

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.use(teamRouter);
 app.use(imageRouter);
 app.use(userRouter);
 // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '/build')));
+//app.use(express.static(path.join(__dirname, '/build'))); 
 
 // Handle React routing, return all requests to React app
 // app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/build', 'index.html'));
+// res.sendFile(path.join(__dirname, '/build', 'index.html'));
 // });
 
 // Start server
